4db67ba2efd2e61945816aa9bf803b3b71296905
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-09 20:28:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -111,4 +111,15 @@ class Student
       self.new_from_db(row)
     end.first
   end
+  
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = #{x}
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
