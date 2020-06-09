2a2f2251b164efe8b32a16aca12e85727fc8d45d
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-09 20:21:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -108,9 +108,7 @@ class Student
     LIMIT 1
     SQL
     DB[:conn].execute(sql).map do |row|
-      student = self.new_from_db(row)
-      student[0]
-      binding.pry
+      self.new_from_db(row)
     end
   end
 end
